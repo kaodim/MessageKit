a8a9858c9b9ec48b2f76369be8fd238eb345434e
KrishBaddi <ramkrishnabaddi@gmail.com>
2019-04-29 18:09:13 +0800

    - Bumped version - Label padding

--- Sources/Views/Headers & Footers/MessageHeaderLineView.swift
+++ Sources/Views/Headers & Footers/MessageHeaderLineView.swift
@@ -12,7 +12,7 @@ open class MessageLineHeaderView: MessageHeaderView {
 
     // MARK: - Properties
 
-    open let titleLabel = UILabel()
+    let titleLabel = UILabel()
 
     // MARK: - Initializers
 
@@ -26,18 +26,32 @@ open class MessageLineHeaderView: MessageHeaderView {
         titleLabel.font = UIFont.systemFont(ofSize: 12.0, weight: .semibold)
         titleLabel.textColor = UIColor.color(0x9b9b9b)
         titleLabel.backgroundColor = .clear
-        addSubview(titleLabel)
-    }
 
-    open override func layoutSubviews() {
-        super.layoutSubviews()
+
+        addSubview(titleLabel)
 
         self.backgroundColor = UIColor.color(0xf2f2f2)
         NSLayoutConstraint.activate([
-            titleLabel.heightAnchor.constraint(equalToConstant: bounds.height + 26),
+            titleLabel.heightAnchor.constraint(equalToConstant: bounds.height),
             titleLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 50.0),
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor), titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
             ])
+
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .white
+        contentView.layer.cornerRadius = 17.0
+        addSubview(contentView)
+
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -10),
+            contentView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 10),
+             contentView.heightAnchor.constraint(equalTo: titleLabel.heightAnchor, constant: -16),
+            contentView.centerXAnchor.constraint(equalTo: centerXAnchor), contentView.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+
+        self.bringSubview(toFront: titleLabel)
+
     }
 
     public required init?(coder aDecoder: NSCoder) {
